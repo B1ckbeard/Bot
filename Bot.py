1b5fc206ce8adb0cb3f1c1b2e8358b2e9de47887
Black_Beard <evs.diman@yandex.ru>
2021-09-06 18:05:26 +0300

    0.0.2

--- Bot.py
+++ Bot.py
@@ -27,53 +27,51 @@ def answer(call):
     if call.data == 'last':
         bot.send_message(call.message.chat.id, Com_service.select())
     elif call.data == 'add':
-        markup_inline1 = types.InlineKeyboardMarkup()
-        item3 = types.InlineKeyboardButton(text = 'Light', callback_data= 'light')
-        item4 = types.InlineKeyboardButton(text = 'Cold_Water', callback_data= 'c_water')
-        item5 = types.InlineKeyboardButton(text = 'Hot_Water', callback_data= 'h_water')
-        item6 = types.InlineKeyboardButton(text = 'Save', callback_data= 'save')
-
-        markup_inline1.add(item3, item4, item5, item6)
-        bot.send_message(call.message.chat.id, 'Выберите:', reply_markup = markup_inline1)
-    elif call.data == 'light':
-        sent = bot.send_message(call.message.chat.id, 'Введите показание счетчика электроэнергии:')
-        bot.register_next_step_handler(sent, electricityIndication)
-    elif call.data == 'c_water':
-        sent = bot.send_message(call.message.chat.id, 'Введите показание счетчика холодной воды:')
-        bot.register_next_step_handler(sent, coldWaterIndication)
-    elif call.data == 'h_water':
-        sent = bot.send_message(call.message.chat.id, 'Введите показание счетчика горячей воды:')
-        bot.register_next_step_handler(sent, hotWaterIndication)
-    elif call.data == 'save':
+        mes1 = bot.send_message(call.message.chat.id, 'Введите показание счетчика электроэнергии:')
+        bot.register_next_step_handler(mes1, electricityIndication)
+    elif call.data == 'save':#сохранить показания в БД
         bot.send_message(call.message.chat.id, 'Сохранено')
         saveData()
         markup_inline2 = types.InlineKeyboardMarkup()
-        item7 = types.InlineKeyboardButton(text = 'сумма', callback_data= 'price')
-        markup_inline2.add(item7)
+        item3 = types.InlineKeyboardButton(text = 'сумма', callback_data= 'price')
+        markup_inline2.add(item3)
         bot.send_message(call.message.chat.id, 'Посчитать сумму?', reply_markup = markup_inline2)
-    elif call.data == 'price':
+    elif call.data == 'price':#пок. в текущем месяце - пок. пред. месяца * тариф
         sent = bot.send_message(call.message.chat.id, Com_service.priceSum())
 
 def electricityIndication(message):
     global el_ind
     el_ind = message.text
-    bot.send_message(message.chat.id, f'{message.text}, Ok')
+    #bot.send_message(message.chat.id, f'{message.text}, Ok')
+    if message != '':
+        mes2 = bot.send_message(message.chat.id, 'Введите показание счетчика холодной воды:')
+        bot.register_next_step_handler(mes2, coldWaterIndication)
 
 def coldWaterIndication(message):
     global c_water
     c_water = message.text
-    bot.send_message(message.chat.id, f'{message.text}, Ok')
+    #bot.send_message(message.chat.id, f'{message.text}, Ok')
+    if message != '':
+        mes3 = bot.send_message(message.chat.id, 'Введите показание счетчика горячей воды:')
+        bot.register_next_step_handler(mes3, hotWaterIndication)
 
 def hotWaterIndication(message):
     global h_water
     h_water = message.text
-    bot.send_message(message.chat.id, f'{message.text}, Ok')
+    #bot.send_message(message.chat.id, f'{message.text}, Ok')
+    showInditations(message)
+
+def showInditations(message):
+    bot.send_message(message.chat.id, f'свет: {el_ind}, х_вода: {c_water}, г_вода: {h_water}')
+    markup_inline2 = types.InlineKeyboardMarkup()
+    item1 = types.InlineKeyboardButton(text = 'Save', callback_data= 'save')
+    markup_inline2.add(item1)
+    bot.send_message(message.chat.id, 'Сохранить?', reply_markup = markup_inline2)
     
+#Сохранить показания в БД
 def saveData():
     Com_service.db_table_add(float(el_ind), float(c_water), float(h_water))
     print('data saved')
-
-
 
 '''
 @bot.message_handler(content_types=['text'])
@@ -84,6 +82,5 @@ def send_hello(message):
         bot.send_message(message.from_user.id, 'Всего хорошего!')
     elif message.text == 'Мяу':
         bot.send_message(message.from_user.id, 'мау-мау)')'''
-
 
 bot.polling(none_stop=True)
