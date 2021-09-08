ab054216c23e423ee25e30a5f82d8a87a06ee4e3
BlackBeard <evs.diman@yandex.ru>
2021-09-08 13:22:35 +0300

    0.0.8

--- Main.py
+++ Main.py
@@ -2,7 +2,6 @@ import telebot
 #from telebot.apihelper import ENABLE_MIDDLEWARE
 import config
 from telebot import types
-#from telebot.types import *
 
 import Com_service
 #import Com_service_35
@@ -20,9 +19,8 @@ def get_user_info(message):
     markup_inline = types.InlineKeyboardMarkup()
     item1 = types.InlineKeyboardButton(text = 'Last info', callback_data = 'last')
     item2 = types.InlineKeyboardButton(text = 'Last Sum', callback_data = 'price')
-    item3 = types.InlineKeyboardButton(text = 'Delete last', callback_data = 'del')
+    item3 = types.InlineKeyboardButton(text = 'Delete last', callback_data = 'del_question')
     item4 = types.InlineKeyboardButton(text = 'Add', callback_data = 'add')
-    
     markup_inline.add(item1, item2, item3, item4)
     bot.send_message(message.chat.id, 'Выберите:', reply_markup = markup_inline)
 
@@ -31,58 +29,83 @@ def answer(call):
     if call.data == 'last':
         bot.send_message(call.message.chat.id, Com_service.select_last_record())
     elif call.data == 'add':
-        mes1 = bot.send_message(call.message.chat.id, 'Введите показание счетчика электроэнергии:')
-        bot.register_next_step_handler(mes1, electricityIndication)
+        indicationMethodChoice(call.message, 'elec')
     elif call.data == 'save':#сохранить показания в БД
-        saveData()
-        bot.send_message(call.message.chat.id, 'Сохранено')
+        saveData(call.message)
+        #добавить в метод суммы
         markup_inline2 = types.InlineKeyboardMarkup()
-        item3 = types.InlineKeyboardButton(text = 'сумма', callback_data = 'price')
-        markup_inline2.add(item3)
+        item1 = types.InlineKeyboardButton(text = 'сумма', callback_data = 'price')
+        markup_inline2.add(item1)
         bot.send_message(call.message.chat.id, 'Посчитать сумму?', reply_markup = markup_inline2)
     elif call.data == 'price':#пок. в текущем месяце - пок. пред. месяца * тариф
         bot.send_message(call.message.chat.id, Com_service.price_sum())
+    elif call.data == 'del_question':
+        markup_inline3 = types.InlineKeyboardMarkup()
+        item1 = types.InlineKeyboardButton(text= 'Удалить', callback_data= 'del')
+        markup_inline3.add(item1)
+        bot.send_message(call.message.chat.id, 'Вы действительно хотите удалить последнюю запись?', reply_markup=markup_inline3)
     elif call.data == 'del':
-        delData()
-        bot.send_message(call.message.chat.id, 'Последняя запись удалена')
+        delData(call.message)
+
+def indicationMethodChoice(message, met):
+    if met == 'elec':
+        mes1 = bot.send_message(message.chat.id, 'Введите показание счетчика электроэнергии:')
+        bot.register_next_step_handler(mes1, electricityIndication)
+    elif met == 'c_water':
+        mes1 = bot.send_message(message.chat.id, 'Введите показание счетчика холодной воды:')
+        bot.register_next_step_handler(mes1, coldWaterIndication)
+    elif met == 'h_water':
+        mes1 = bot.send_message(message.chat.id, 'Введите показание счетчика горячей воды:')
+        bot.register_next_step_handler(mes1, hotWaterIndication)
 
 def electricityIndication(message):
-    global el_ind
-    el_ind = message.text
-    #bot.send_message(message.chat.id, f'{message.text}, Ok')
-    if message != '':
-        mes2 = bot.send_message(message.chat.id, 'Введите показание счетчика холодной воды:')
-        bot.register_next_step_handler(mes2, coldWaterIndication)
+    try:
+        global el_ind
+        el_ind = float(message.text)
+    except ValueError as error:
+        bot.send_message(message.chat.id, f'Неверный тип данных "{error} ", повторите ввод')
+        indicationMethodChoice(message, 'elec')
+    else:
+        bot.send_message(message.chat.id, f'{message.text}, Ok')
+        indicationMethodChoice(message, 'c_water')
 
 def coldWaterIndication(message):
-    global c_water
-    c_water = message.text
-    #bot.send_message(message.chat.id, f'{message.text}, Ok')
-    if message != '':
-        mes3 = bot.send_message(message.chat.id, 'Введите показание счетчика горячей воды:')
-        bot.register_next_step_handler(mes3, hotWaterIndication)
+    try:
+        global c_water
+        c_water = float(message.text)
+    except ValueError as error:
+        bot.send_message(message.chat.id, f'Неверный тип данных "{error} ", повторите ввод')
+        indicationMethodChoice(message, 'c_water')
+    else:
+        bot.send_message(message.chat.id, f'{message.text}, Ok')
+        indicationMethodChoice(message, 'h_water')
 
 def hotWaterIndication(message):
-    global h_water
-    h_water = message.text
-    #bot.send_message(message.chat.id, f'{message.text}, Ok')
-    showInditations(message)
+    try:
+        global h_water
+        h_water = float(message.text)
+    except ValueError as error:
+        bot.send_message(message.chat.id, f'Неверный тип данных "{error} ", повторите ввод')
+        indicationMethodChoice(message, 'h_water')
+    else:
+        bot.send_message(message.chat.id, f'{message.text}, Ok')
+        showInditations(message)
 
-def showInditations(message):
+def showInditations(message):#показать введенные данные
     bot.send_message(message.chat.id, f'свет: {el_ind}, х_вода: {c_water}, г_вода: {h_water}')
     markup_inline2 = types.InlineKeyboardMarkup()
     item1 = types.InlineKeyboardButton(text = 'Save', callback_data= 'save')
     markup_inline2.add(item1)
     bot.send_message(message.chat.id, 'Сохранить?', reply_markup = markup_inline2)
     
-#Сохранить показания в БД
-def saveData():
+def saveData(message):#Сохранить показания в БД
     Com_service.db_table_add(float(el_ind), float(c_water), float(h_water))
     print('data saved')
+    bot.send_message(message.chat.id, 'Сохранено')
 
-#Удалить последнюю запись
-def delData():
+def delData(message):#Удалить последнюю запись
     Com_service.delete_last_record()
+    bot.send_message(message.chat.id, 'Последняя запись удалена')
 
 @bot.message_handler(content_types=['text'])
 def send_hello(message):
--- others/Dice_roll.py
+++ others/Dice_roll.py
@@ -1,13 +1,9 @@
 import random
 
-
 def dice_roll(x):
     if x == 1:
-        print(random.randint(1, 6))
+        return(random.randint(1, 6))
     elif x == 2:
-        print(random.randint(2, 12))
-
-
-num = int(input())
-
-dice_roll(num)
+        return(random.randint(2, 12))
+    else:
+        pass
